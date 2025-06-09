8f8f4cf0e4eaecfd8d6781189a4932daf48c4a97
Ryan Volkin <ryanvolkin@gmail.com>
2025-06-09 12:11:37 -0500

    added files

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about-page'
+}
+</script>
+
+<style scoped>
+.about {
+  padding: 2rem;
+  color: white;
+  text-align: center;
+}
+</style>
