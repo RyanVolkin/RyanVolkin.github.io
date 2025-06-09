fcf652d4a8b21572115ab8e5dcc5932d9b006f06
Ryan Volkin <126348273+RyanVolkin@users.noreply.github.com>
2025-06-09 11:55:21 -0500

    Add files via upload

---
+++ components/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="projects">
+    <h1>Projects</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'projects-page'
+}
+</script>
+
+<style scoped>
+#projects {
+  padding: 2rem;
+  color: white;
+  text-align: center;
+}
+</style>
